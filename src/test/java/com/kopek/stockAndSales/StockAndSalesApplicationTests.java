968582d53f9034ca31857cbcfc62b4b93d46f6a7
dlufiego <diegolufiego@gmail.com>
2024-02-17 13:33:11 -0300

    commit

---
+++ src/test/java/com/kopek/stockAndSales/StockAndSalesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kopek.stockAndSales;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockAndSalesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
